511f8ac81a08e62f2b3292bb4e28959be0b0b08a
zumerahmadbajwa <zumerahmad02@gmail.com>
2024-08-15 10:26:11 +0500

    table drops

---
+++ db/migrate/20240813053827_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.references :assessment, null: false, foreign_key: true
+      t.text :content, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_12_055903) do
+ActiveRecord::Schema.define(version: 2024_08_15_051852) do
 
   create_table "assessments", force: :cascade do |t|
     t.string "title"
