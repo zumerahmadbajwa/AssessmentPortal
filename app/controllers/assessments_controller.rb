ca655e34f63c818d63032c9bd431b0948757afc2
zumerahmadbajwa <zumerahmad02@gmail.com>
2024-08-21 16:49:26 +0500

    Fixing Bugs

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -2,18 +2,22 @@
 class AssessmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_assessment, only: [:show, :attempt, :submit, :results]
-
+  
   def show
     @questions = @assessment.questions.includes(:options)
   end
 
   def attempt
+    authorize @assessment
     @questions = @assessment.questions.includes(:options)
+    @assessments = current_user.assessments.find(params[:id])
   end
 
   def submit
-    answers = params[:answers]  # Hash with question_id as key and selected option_id as value
+    answers = params[:answers]
+    puts "Answers received: #{answers.inspect}" 
     score = calculate_score(answers)
+    flash[:notice] = "Your score: #{score}"
     Result.create!(user: current_user, assessment: @assessment, score: score, answers: answers.to_json)
     redirect_to results_assessment_path(@assessment), notice: 'Assessment submitted successfully.'
   end
@@ -33,17 +37,24 @@ class AssessmentsController < ApplicationController
 
   def calculate_score(answers)
     correct_answers = 0
-
+  
+    puts "Answers received: #{answers.inspect}"
+  
     @assessment.questions.each do |question|
-      selected_option_id = answers[question.id.to_s]
+      selected_option_id = answers[question.id.to_s] || answers[question.id.to_s] || answers[question.id]
       correct_option = question.options.find_by(correct: true)
-
+  
+      puts "Question ID: #{question.id}"
+      puts "Selected Option ID: #{selected_option_id}"
+      puts "Correct Option ID: #{correct_option&.id}"
+  
       if correct_option && selected_option_id.to_i == correct_option.id
         correct_answers += 1
       end
     end
-
-    # Assuming each question scored 10 points
+  
+    puts "Total Correct Answers: #{correct_answers}"
+  
     correct_answers * 10
   end
 end
