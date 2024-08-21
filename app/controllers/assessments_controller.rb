8111f857ac218858f789fe35c39cead4aa1e0496
zumerahmadbajwa <zumerahmad02@gmail.com>
2024-08-21 17:22:04 +0500

    Resolve some logical issues

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -14,16 +14,18 @@ class AssessmentsController < ApplicationController
   end
 
   def submit
-    answers = params[:answers]
-    puts "Answers received: #{answers.inspect}" 
-    score = calculate_score(answers)
-    flash[:notice] = "Your score: #{score}"
-    Result.create!(user: current_user, assessment: @assessment, score: score, answers: answers.to_json)
-    redirect_to results_assessment_path(@assessment), notice: 'Assessment submitted successfully.'
+    user_answers = permitted_params[:answers] || {}
+
+    serialized_answers = user_answers.to_json # Serialize answers to JSON
+
+    @score = calculate_score(user_answers)
+
+    redirect_to results_assessment_path(@assessment, score: @score, answers: serialized_answers)
   end
 
   def results
-    @result = Result.find_by(user: current_user, assessment: @assessment)
+    @score = params[:score].to_i
+    @answers = JSON.parse(params[:answers] || '{}') 
   end
 
   private
@@ -35,26 +37,22 @@ class AssessmentsController < ApplicationController
     redirect_to assessments_path
   end
 
+  def permitted_params
+    params.permit(:id, answers: {})
+  end
+
   def calculate_score(answers)
     correct_answers = 0
-  
-    puts "Answers received: #{answers.inspect}"
-  
+
     @assessment.questions.each do |question|
-      selected_option_id = answers[question.id.to_s] || answers[question.id.to_s] || answers[question.id]
+      selected_option_id = answers[question.id.to_s]
       correct_option = question.options.find_by(correct: true)
-  
-      puts "Question ID: #{question.id}"
-      puts "Selected Option ID: #{selected_option_id}"
-      puts "Correct Option ID: #{correct_option&.id}"
-  
+
       if correct_option && selected_option_id.to_i == correct_option.id
         correct_answers += 1
       end
     end
-  
-    puts "Total Correct Answers: #{correct_answers}"
-  
+
     correct_answers * 10
   end
 end
